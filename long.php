4f17752acc9b7c54ba679291bf24cb7d354f0f4f
alice <alice@example.org>
2012-12-31 15:21:03 +0100

    add a long file

---
+++ long.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This file is a long PHP file.
+ *
+ * It's used for tests, because we want to be sure that codeviewer displays the
+ * whole file with a proper height.
+ */
+
+$foo = "bar";
+
+$x = '';
+for ($i = 1; $i < 1000; $i++) {
+    $x .= $foo;
+}
+
+$count = function ($string) {
+    return count(explode('b', $string));
+};
+
+$j = $count($x);
+
+$foo = "bar";
+
+$x = '';
+for ($i = 1; $i < 1000; $i++) {
+    $x .= $foo;
+}
+
+$count = function ($string) {
+    return count(explode('b', $string));
+};
+
+$j = $count($x);
+
+
+$foo = "bar";
+
+$x = '';
+for ($i = 1; $i < 1000; $i++) {
+    $x .= $foo;
+}
+
+$count = function ($string) {
+    return count(explode('b', $string));
+};
+
+$j = $count($x);
+
+
+$foo = "bar";
+
+$x = '';
+for ($i = 1; $i < 1000; $i++) {
+    $x .= $foo;
+}
+
+$count = function ($string) {
+    return count(explode('b', $string));
+};
+
+$j = $count($x);
+
+
+$foo = "bar";
+
+$x = '';
+for ($i = 1; $i < 1000; $i++) {
+    $x .= $foo;
+}
+
+$count = function ($string) {
+    return count(explode('b', $string));
+};
+
+$j = $count($x);
+
+
+
